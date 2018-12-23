54e5711c23b76226bc121ff113468ca3cc6e545f
asaadawi <asaadawi@thiqah.sa>
2018-12-23 14:30:47 +0300

    this is my first commit

---
+++ Bill.cs
@@ -0,0 +1,111 @@
+﻿using PartnersDecisions.Core.Basis;
+using PartnersDecisions.GraphDiff.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartnersDecisions.Core
+{
+    [Serializable]
+    [Table("Bill", Schema = "pd")]
+    public class Bill : AuditableEntity
+    {
+        #region  Contractors
+
+        public Bill()
+        {
+
+        }
+
+        public Bill(string sadadNumber, int daysToExpire, string billerName, Guid referenceNumber, List<BillDetail> details)
+        {
+            this.SadadNumber = sadadNumber;
+            double sumVATAmount = details.Sum(d => d.VATAmount);
+            double sumAmount = details.Sum(d => d.Amount);
+            this.TotalAmount = sumVATAmount + sumAmount;
+            this.BillerName = billerName;
+            this.Description = billerName;
+            this.ReferenceNumber = referenceNumber;
+            this.ExpirationDate = DateTime.Now.AddDays(daysToExpire);
+
+            this.BillDetails = details;
+            this.SetCreated();
+        }
+
+        public Bill(double amount, string description)
+        {
+            this.TotalAmount = amount;
+            this.Description = description;
+        }
+        #endregion
+
+        #region Properties
+        [Key]
+        public int BillID { get; private set; }
+
+        [StringLength(50)]
+        public string SadadNumber { get; private set; }
+
+        public double TotalAmount { get; private set; }
+
+        public DateTime? SadadDate { get; private set; }
+
+        public DateTime? ExpirationDate { get; private set; }
+
+        [StringLength(1000)]
+        public string Description { get; private set; }
+
+        public string BillerName { get; private set; }
+
+        public Guid ReferenceNumber { get; private set; }
+
+        [Owned]
+        public List<BillDetail> BillDetails { get; private set; }
+
+        #endregion
+
+        #region Public Methods
+
+        public Bill UpdateBill(string sadadNumber, DateTime expirationDate)
+        {
+            this.SadadNumber = sadadNumber;
+            this.ExpirationDate = expirationDate;
+            this.SetUpdated();
+            return this;
+        }
+
+        public Bill SetBillPaid(DateTime SadadDate)
+        {
+            this.SadadDate = SadadDate;
+            this.SetUpdated();
+            return this;
+        }
+
+        public Bill SetDetialsBill(int invoiceID)
+        {
+            this.BillID = invoiceID;
+            this.BillDetails.ForEach(d => { d.SetBillID(invoiceID); });
+            this.SetUpdated();
+            return this;
+        }
+
+        public Bill UpdateBillDetails(List<BillDetail> billDetails)
+        {
+            this.BillDetails = billDetails;
+            this.SetUpdated();
+            return this;
+        }
+
+        //public Bill SetBillCancelled()
+        //{
+        //    this.IsValid = false;
+        //    this.SetUpdated();
+        //    return this;
+        //}
+        #endregion
+    }
+}
---
+++ CapitalReductionAnnouncement.cs
@@ -0,0 +1,52 @@
+﻿using PartnersDecisions.GraphDiff.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PartnersDecisions.Core
+{
+    [Serializable]
+    [Table("CapitalReductionAnnouncement", Schema = "pd")]
+    public class CapitalReductionAnnouncement : PartnerDecision
+    {
+        #region Constractors
+
+        public CapitalReductionAnnouncement() : base()
+        {
+
+        }
+
+        public CapitalReductionAnnouncement(Contract contract, float newCapital, DateTime meetingDate, string MeetingDateHijri, string reductionReason, List<PartnerDecisionApproval> Approvals = null, string DecisionText = "")
+            : base(contract, Enums.PartnerDecisionStatusEnum.WaitingPartnersApprovals)
+        {
+            this.NewCapital = newCapital;
+            this.MeetingDate = meetingDate;
+            this.MeetingDateHijri = MeetingDateHijri;
+            this.ContractID = contract.ContractID;
+            this.ReductionReason = reductionReason;
+            this.CreatedAt = DateTime.Now;
+            this.PartnerDecisionApprovals = Approvals;
+            this.DecisionText = DecisionText;
+            this.PartnerDecisionStatus = Enums.PartnerDecisionStatusEnum.WaitingPartnersApprovals;
+            this.SetCreated();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public DateTime MeetingDate { get; protected set; }
+
+        [StringLength(10)]
+        public string MeetingDateHijri { get; protected set; }
+
+        public float NewCapital { get; protected set; } 
+
+        [StringLength(3000)]
+        public string ReductionReason { get; protected set; }
+
+
+        #endregion
+    }
+}

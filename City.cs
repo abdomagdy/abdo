70f37c685565688c246c83b7f3bf3083a388eb1c
asaadawi <asaadawi@thiqah.sa>
2019-01-22 12:54:18 +0300

    abdo323

--- City.cs
+++ City.cs
@@ -8,7 +8,7 @@ namespace PartnersDecisions.Core
 {
     public class City
     {
-        public int CityID { get; private set; }
-        public string Name { get; private set; }
+        public int CityID { get; private set; }         // this is the new ,pdofo,ca
+ //       public string Name { get; private set; }
     }
 }
